4c7e43cc3864ff9214ec0632be39a92cb76697dd
Tue Do <tuedo2@illinois.edu>
2022-11-16 19:05:04 -0600

    get constants

--- pages/api/gradient.ts
+++ pages/api/gradient.ts
@@ -16,10 +16,10 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   await sleep(2000)
-  var featuresInfo = getFeaturesInfo('71uF3xQj2v6DTZyCRIvZHv');
-
+  var featuresInfo = await getFeaturesInfo('71uF3xQj2v6DTZyCRIvZHv');
+  console.log(featuresInfo)
   // red
-  var energy = featuresInfo['energy']
+  var energy = featuresInfo['energy'];
   var valence = featuresInfo['valence']
   var loudness = featuresInfo['loudness']
   var liveness = featuresInfo['liveness']
@@ -27,10 +27,9 @@ export default async function handler(
   var c2_red = 256 * (0.4 * energy[1] + 0.3 * valence[1] + 0.2 * loudness[1] + 0.1 * liveness[1])
   var c_red = (energy[2] - energy[0] + valence[2] - valence[0] + loudness[2] - loudness[0] + liveness[2] - liveness[0])
               / (energy[0] - energy[1] + valence[0] - valence[1] + loudness[0] - loudness[1] + liveness[0] - liveness[1])
-  res.status(200).json({data: {color: 'red', c1: c1_red, c2: c2_red, c: c_red}})
 
   // green
-  var acousticness = featuresInfo['accousticness']
+  var acousticness = featuresInfo['acousticness']
   var instrumentalness = featuresInfo['instrumentalness']
   var speechiness = featuresInfo['speechiness']
   var danceability = featuresInfo['danceability']
@@ -38,7 +37,6 @@ export default async function handler(
   var c2_green = 256 * (0.4 * danceability[1] + 0.2 * speechiness[1] + 0.2 * instrumentalness[1] + 0.2 * acousticness[1])
   var c_green = (acousticness[2] - acousticness[0] + instrumentalness[2] - instrumentalness[0] + speechiness[2] - speechiness[0] + danceability[2] - danceability[0])
                 / (acousticness[0] - acousticness[1] + instrumentalness[0] - instrumentalness[1] + speechiness[0] - speechiness[1] + danceability[0] - danceability[1])
-  res.status(200).json({data: {color: 'green', c1: c1_green, c2: c2_green, c: c_green}})
 
   // blue
   var mode = featuresInfo['mode']
@@ -48,5 +46,14 @@ export default async function handler(
   var c1_blue = 256 * (0.4 * mode[2] + 0.4 * tempo[2] + 0.1 * key[2] + 0.1 * time_signature[2])
   var c2_blue = 256 * (0.4 * mode[1] + 0.4 * tempo[1] + 0.1 * key[1] + 0.1 * time_signature[1])
   var c_blue = (tempo[2] - tempo[0]) / (tempo[0] - tempo[1])
-  res .status(200).json({data: {color: 'blue', c1: c1_blue, c2: c2_blue, c: c1_blue}})
+
+  console.log('red', c1_red, c2_red, c_red)
+  console.log('green', c1_green, c2_green, c_green)
+  console.log('blue', c1_blue, c2_blue, c_blue)
+
+  res.status(200).json({data: {
+    red: {c1: c1_red, c2: c2_red, c: c_red},
+    green: {c1: c1_green, c2: c2_green, c: c_green},
+    blue: {c1: c1_blue, c2: c2_blue, c: c_blue}
+  }})
 }
--- utils/trackFeatures.ts
+++ utils/trackFeatures.ts
@@ -4,7 +4,7 @@ const calculateMeanQuartile = (arr) => {
   const asc = arr => arr.sort((a, b) => a - b);
   const sum = arr => arr.reduce((a, b) => a + b, 0);
   const mean = arr => sum(arr) / arr.length;
-  
+
   const quantile = (arr, q) => {
       const sorted = asc(arr);
       const pos = (sorted.length - 1) * q;
@@ -18,70 +18,55 @@ const calculateMeanQuartile = (arr) => {
   };
   const q25 = (arr) => quantile(arr, .25);
   const q75 = arr => quantile(arr, .75);
-  
+
   l.push(mean(arr), q25(arr), q75(arr));
   return l;
 }
 
-const getFeaturesInfo = (playlist) => {
+export const getFeaturesInfo = async (playlist) => {
+  const SpotifyWebApi = require('../node_modules/spotify-web-api-node');
 
-    const SpotifyWebApi = require('../node_modules/spotify-web-api-node');
+  const spotifyApi = new SpotifyWebApi({
+      accessToken: 'BQDzv9Jfd8o2qeWs6gkjf2WhcYldLYqsQXNgfHxYiyQsjhhJaEZq_ScNJu1fVXnhJOUOvPj6QknnGy5isRrEGenAp0NSJFazOwXDlUAmLsP7Bo2RGCjuTe_jiaKLy3jMDXXH2stA94GPJgl6xnDuhmPLCvWj2psVo2rXMFbqakLM'
+  });
 
-    const spotifyApi = new SpotifyWebApi({
-        accessToken: 'BQDpYxhsRFYTkZznkdujDpXmArfBosjc9F9noiBCwDxJUkODcUJr5XdaMxYvQEkOnxLQ9JT9iglvC3OEgD_LqbE02RsTJxPd3-PZGlTeQP4THvAJAbKSVnnEWKu4KdtAcWHs1kRD0vAPXT0PpXyfDG4XV_TEKmOshnTY7F6BzLR1'
-    });
+  var tracks: string[] = [];
+  var features=['danceability','energy','key','loudness','speechiness','acousticness','instrumentalness',
+                  'liveness','valence','tempo','time_signature','mode'];
+  var featureValues = {};
+  var featuresInfo = {};
 
-    var tracks = [];
-    var features=['danceability','energy','key','loudness','speechiness','acousticness','instrumentalness',
-                    'liveness','valence','tempo','time_signature','mode'];
-    var featureValues = {};
-    var featuresInfo = {};
+  const data = await spotifyApi.getPlaylistTracks(playlist, {
+      offset: 1,
+      limit: 5,
+      fields: 'items'
+  })
 
-    spotifyApi.getPlaylistTracks(playlist, {
-        offset: 1,
-        limit: 5,
-        fields: 'items'
-      })
-      .then(
-        function(data) {
-          var items = data.body.items;
-          //console.log('Items : ', items);
-          items.forEach(element => {
-            //console.log('TrackId : ', element.track.id);
-            tracks.push(element.track.id);
-          });
-          console.log('The playlist contains these tracks', tracks);
-          
-          spotifyApi.getAudioFeaturesForTracks(tracks)
-            .then(
-                function(data) {
-                    //console.log('data : ', data.body);
-                    var audioFeatures=data.body.audio_features;
-                    audioFeatures.forEach(element => {
-                        features.forEach(feature => {
-                            //console.log('feature name :',element[feature]);
-                            featureValues[feature] = featureValues[feature] || [];
-                            featureValues[feature].push(element[feature]);
-                        });
-                    });
+  var items = data.body.items;
+  //console.log('Items : ', items);
+  items.forEach(element => {
+    //console.log('TrackId : ', element.track.id);
+    tracks.push(element.track.id);
+  });
+  console.log('The playlist contains these tracks', tracks);
 
-                    console.log('featureValues ::: ', featureValues);
-                    
-                    for (const [key, value] of Object.entries(featureValues)) {
-                      featuresInfo[key] = calculateMeanQuartile(featureValues[key]);
-                    }
+  const featureData = await spotifyApi.getAudioFeaturesForTracks(tracks)
+  //console.log('data : ', data.body);
+  var audioFeatures=featureData.body.audio_features;
+  audioFeatures.forEach(element => {
+      features.forEach(feature => {
+          //console.log('feature name :',element[feature]);
+          featureValues[feature] = featureValues[feature] || [];
+          featureValues[feature].push(element[feature]);
+      });
+  });
 
-                    console.log('featuresInfo ::: ', featuresInfo);
-                },
-                function(err) {
-                    console.log('Something went wrong!', err);
-                }
-          );
-        },
-        function(err) {
-          console.log('Something went wrong!', err);
-        }
-      );
+  // console.log('featureValues ::: ', featureValues);
+
+  for (const [key, value] of Object.entries(featureValues)) {
+    featuresInfo[key] = calculateMeanQuartile(featureValues[key]);
+  }
+
+  // console.log('featuresInfo ::: ', featuresInfo);
+  return featuresInfo
 };
-
-getFeaturesInfo('71uF3xQj2v6DTZyCRIvZHv');
