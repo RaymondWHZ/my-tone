eec664312ae5ff401f80787dfdaddcce2e5a2d64
Tue Do <tuedo2@illinois.edu>
2022-11-16 18:11:18 -0600

    red formula

--- pages/api/gradient.ts
+++ pages/api/gradient.ts
@@ -17,5 +17,15 @@ export default async function handler(
 ) {
   await sleep(2000)
   var featuresInfo = getFeaturesInfo('71uF3xQj2v6DTZyCRIvZHv');
-  res.status(200).json({ data: { color: "red" } })
+
+  // red
+  var energy = featuresInfo['energy']
+  var valence = featuresInfo['valence']
+  var loudness = featuresInfo['loudness']
+  var liveness = featuresInfo['liveness']
+  var c1_red = 256 * (0.4 * energy[2] + 0.3 * valence[2] + 0.2 * loudness[2] + 0.1 * liveness[2])
+  var c2_red = 256 * (0.4 * energy[1] + 0.3 * valence[1] + 0.2 * loudness[1] + 0.1 * liveness[1])
+  var c_red = (energy[2] - energy[0] + valence[2] - valence[0] + loudness[2] - loudness[0] + liveness[2] - liveness[0])
+              / (energy[0] - energy[1] + valence[0] - valence[1] + loudness[0] - loudness[1] + liveness[0] - liveness[1])
+  res.status(200).json({data: {color: 'red', c1: c1_red, c2: c2_red, c: c_red}})
 }
